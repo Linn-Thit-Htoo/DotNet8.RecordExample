633670ad2a720dfc3e134ca8f4f5de3999fe8d0e
Linn Thit <lth1212001@gmail.com>
2024-05-09 14:40:21 +0630

    Add project files.

---
+++ DotNet8.RecordExample/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DotNet8.RecordExample;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Product product = new(10, "Product Sample", 10000, 10, true);
+        Console.WriteLine(product.ProductName);
+
+        ExampleModel model = new(10, "Linn Thit");
+    }
+}
+
+public record Product(long ProductId, string ProductName, double Price, int Stock, bool IsActive);
+
+public class ExampleModel
+{
+    public ExampleModel(long id, string name)
+    {
+        UserId = id;
+        UserName = name;
+    }
+
+    public long UserId { get; set; }
+    public string UserName { get; set; } = null!;
+}
